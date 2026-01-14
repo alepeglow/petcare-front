b8f083dd5ae50c28225dc7df6e05787b293ccc92
Alessandra Peglow <peglowalessandra@gmail.com>
2026-01-14 17:20:10 -0300

    app petcare

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './layout/shell/shell';
+import { DashboardComponent } from './pages/dashboard/dashboard';
+import { PetsComponent } from './pages/pets/pets';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'pets', component: PetsComponent },
+      // depois: adocoes, adotantes, agendamentos...
+    ],
+  },
+];
+
