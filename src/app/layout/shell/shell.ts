8e39b70d190b6a1eb104c06be259e07d87dd0a04
Alessandra Peglow <peglowalessandra@gmail.com>
2026-01-14 17:30:38 -0300

    app petcare

--- src/app/layout/shell/shell.ts
+++ src/app/layout/shell/shell.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-shell',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+  ],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
 export class Shell {
-
+  year = new Date().getFullYear();
 }
